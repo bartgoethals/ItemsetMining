b1ece2ba9ba6cf06ae9da517cc5e19921c0d759d
bartgoethals <goethals@gmail.com>
2014-12-13 11:42:16 +0100

    Create data.py
    First attempt at data class

---
+++ data.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+""" 
+author: Bart Goethals
+
+This class contains the necessary code to read the data in memory in a simple data structure.
+This can be based on the tid-lists, diff-sets, or even simple transactions as being used by
+most frequent itemset mining algorithms
+"""
+
+class Data:
+
+  def __init__(self):
+    self.data = {}
+    self.trans = 0
+
+  def readTidLists(self, filename):
+    f = open(filename, 'r')
+    for row in f:
+      s = row.strip().split(' ')
+      trans+=1
+      for item in s:
+        if item not in data:
+          data[item] = set()
+        data[item].add(trans)
+    f.close()
